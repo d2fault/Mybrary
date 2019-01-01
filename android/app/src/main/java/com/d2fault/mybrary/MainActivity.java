46531539e44733e0dda037016696048c73bee29b
d2fault <realplay23@naver.com>
2019-01-01 13:09:20 +0900

    [ADD] Volley + Kakao book API를 이용한 ISBN 검색(Log로만 확인 가능)

--- android/app/src/main/java/com/d2fault/mybrary/MainActivity.java
+++ android/app/src/main/java/com/d2fault/mybrary/MainActivity.java
@@ -9,13 +9,27 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     private final Activity activity = this;
     private Button buttonFind;
+    private String isbnStr;
     private final String TAG = "MainActivity";
 
     @Override
@@ -51,9 +65,10 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "Cancelled scan");
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
             } else {
-                String isbnStr = result.getContents();
+                isbnStr = result.getContents();
                 if (result.getFormatName().startsWith("EAN_13")) {
                     Toast.makeText(this, isbnStr, Toast.LENGTH_SHORT).show();
+                    getBookInfo();
                 } else {
                     Log.e(TAG, "ISBN ERROR: EAN_13 형식이 아닙니다.\n인식 바코드: " + isbnStr);
                     Toast.makeText(this, "ISBN을 인식하지 못했습니다", Toast.LENGTH_SHORT).show();
@@ -62,5 +77,34 @@ public class MainActivity extends AppCompatActivity {
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void getBookInfo() {
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        String url = getString(R.string.get_book_info_url) + isbnStr + "&target=isbn";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+                    Log.d(TAG, response.toString());
+                }
+            },
+            new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String>  headers = new HashMap<String, String>();
+                headers.put("Authorization", getString(R.string.kakao_api_key));
+
+                return headers;
+            }
+        };
+        //jsonObjectRequest.setTag("J");
+        requestQueue.add(jsonObjectRequest);
     }
 }
