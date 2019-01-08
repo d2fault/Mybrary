5610d0481942307a3736c8b9555e9b7cc4526355
d2fault <realplay23@naver.com>
2019-01-08 17:46:27 +0900

    [CHANGE] MainActivity를 DrawerMenu로 수정

--- android/app/src/main/java/com/d2fault/mybrary/view/MainActivity.java
+++ android/app/src/main/java/com/d2fault/mybrary/view/MainActivity.java
@@ -1,46 +1,99 @@
 package com.d2fault.mybrary.view;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import com.d2fault.mybrary.R;
+import com.d2fault.mybrary.utils.BackPressCloseHandler;
+import com.d2fault.mybrary.utils.BookInfoUtils;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private BackPressCloseHandler backPressCloseHandler;
+    private Toolbar toolbar;
+    private DrawerLayout drawer;
+    private Fragment fragment;
+    private FragmentTransaction ft;
+    private ActionBarDrawerToggle toggle;
+    private NavigationView navigationView;
+    private String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        initModel();
+        initView();
+        initController();
+        initListener();
+
+        ft.replace(R.id.content, fragment);
+        ft.commit();
+    }
+
+    private void initModel() {
+        fragment = new AddBookInfoFragment();
+        title = getString(R.string.app_name);
+        ft = getSupportFragmentManager().beginTransaction();
+    }
+
+    private void initView() {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+    private void initController() {
+        backPressCloseHandler = new BackPressCloseHandler(this);
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+        toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+    }
+
+    private void initListener() {
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("onActivityResult", "Cancelled scan");
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
+            } else {
+                String isbnStr = result.getContents();
+                if (result.getFormatName().startsWith("EAN_13")) {
+                    Toast.makeText(this, isbnStr, Toast.LENGTH_SHORT).show();
+                    BookInfoUtils.getBookInfo(this, isbnStr);
+                } else {
+                    Log.e("onActivityResult", "ISBN ERROR: EAN_13 형식이 아닙니다.\n인식 바코드: " + isbnStr);
+                    Toast.makeText(this, "ISBN을 인식하지 못했습니다", Toast.LENGTH_SHORT).show();
+                }
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
     @Override
@@ -49,7 +102,7 @@ public class MainActivity extends AppCompatActivity
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
-            super.onBackPressed();
+            backPressCloseHandler.onBackPressed();
         }
     }
 
@@ -79,7 +132,9 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         int id = item.getItemId();
+        ft = getSupportFragmentManager().beginTransaction();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
@@ -94,8 +149,6 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_send) {
 
         }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
