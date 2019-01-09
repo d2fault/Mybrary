065019b1da6ecdaa1d20dd1d5c1dce553ec56fba
d2fault <realplay23@naver.com>
2019-01-09 15:27:37 +0900

    [CHANGE] Button을 FloatingActionButton으로 변경

--- android/app/src/main/java/com/d2fault/mybrary/view/MainActivity.java
+++ android/app/src/main/java/com/d2fault/mybrary/view/MainActivity.java
@@ -1,5 +1,6 @@
 package com.d2fault.mybrary.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,7 @@ import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -24,6 +26,9 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private static final String TAG = "[Mybrary] MainActivity - ";
+    private AlertDialog.Builder addBookInfoDialogBuilder;
+    private AlertDialog addBookInfoDialog;
     private BackPressCloseHandler backPressCloseHandler;
     private Toolbar toolbar;
     private DrawerLayout drawer;
@@ -50,6 +55,25 @@ public class MainActivity extends AppCompatActivity
         fragment = new AddBookInfoFragment();
         title = getString(R.string.app_name);
         ft = getSupportFragmentManager().beginTransaction();
+
+        addBookInfoDialogBuilder = new AlertDialog.Builder(this);
+        addBookInfoDialogBuilder.setTitle("책 정보 등록");
+        addBookInfoDialogBuilder
+                .setMessage("책 정보를 등록하시겠습니까?")
+                .setCancelable(false)
+                .setPositiveButton("등록",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        })
+                .setNegativeButton("취소",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+        addBookInfoDialog = addBookInfoDialogBuilder.create();
     }
 
     private void initView() {
@@ -57,6 +81,7 @@ public class MainActivity extends AppCompatActivity
 
         drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         navigationView = (NavigationView) findViewById(R.id.nav_view);
+
 
         setSupportActionBar(toolbar);
     }
@@ -79,15 +104,16 @@ public class MainActivity extends AppCompatActivity
         IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
-                Log.d("onActivityResult", "Cancelled scan");
+                Log.d(TAG, "Cancelled scan");
                 Toast.makeText(this, "Cancelled", Toast.LENGTH_SHORT).show();
             } else {
                 String isbnStr = result.getContents();
                 if (result.getFormatName().startsWith("EAN_13")) {
                     Toast.makeText(this, isbnStr, Toast.LENGTH_SHORT).show();
-                    BookInfoUtils.getBookInfo(this, isbnStr);
+                    BookInfoUtils.getBookInfoFromISBN(this, isbnStr);
+                    addBookInfoDialog.show();
                 } else {
-                    Log.e("onActivityResult", "ISBN ERROR: EAN_13 형식이 아닙니다.\n인식 바코드: " + isbnStr);
+                    Log.e(TAG, "ISBN ERROR: EAN_13 형식이 아닙니다.\n인식 바코드: " + isbnStr);
                     Toast.makeText(this, "ISBN을 인식하지 못했습니다", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -124,7 +150,6 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
